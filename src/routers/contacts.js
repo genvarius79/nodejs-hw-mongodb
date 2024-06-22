7200a6bd99d2095416d8e54cb99b2fd03255ccb5
Hennadii Polupan <146132514+genvarius79@users.noreply.github.com>
2024-06-22 13:49:54 +0100

    create and delete contacts

---
+++ src/routers/contacts.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import {
+  getContactsController,
+  getContactByIdController,
+  createContactController,
+  deleteContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+export default router;
