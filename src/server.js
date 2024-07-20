edbeb85623db9e0f8025fa246c2c597f15f2b3a7
Hennadii Polupan <polupan.gennadiy@gmail.com>
2024-07-20 16:25:35 +0100

    step2-7

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
@@ -31,6 +32,7 @@ export const setupServer = () => {
 
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
 
   app.use('*', notFoundHandler);
